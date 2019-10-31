edab73cb76a23dcc9a8851e98a6cb3df3968a570
GayaChoi <woosung827@naver.com>
2019-10-31 23:09:00 +0900

    checkboxes 수정

--- forms/js/app.js
+++ forms/js/app.js
@@ -1,24 +1,24 @@
 /* checkbox event */
-
 var icon = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewbox="0 0 24 24" class="check">
                 <path d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z"></path>
             </svg>`;
 
-            
-$('#checked').each(function(index) {
-    $('.checkbox-check-icon').eq(index).append(icon);
 
-});
-            
-            
-if ($('#checked').is(':checked')) {
-   $('.checkbox-check-icon').append(icon);
-}
+var checkfunc = function(index) {
+            if ($('.mars-checkbox').eq(index).is(':checked')) {
+                 $('.checkbox-check-icon').eq(index).append(icon);
+            } else {
+                 $('.check').eq(index).remove();
+            }                 
+        };
 
-$('#defualt').on('click',function() {
-    if ($(this).is(':checked')) {
-        $('.checkbox-check-icon').append(icon);
-    } else {
-        $('.check').remove();
-    }
+
+$('.mars-checkbox').each(function(index) { 
+    /* 체크가 되었는 지 확인 */
+    checkfunc(index);
+    
+    /* click event */   
+    $(this).eq(index).on('click',function() {
+        checkfunc(index);  
+    });    
 });
