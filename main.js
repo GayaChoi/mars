545d8d9e6bbddaa286427f7cc5c03d0a5d27121e
GayaChoi <woosung827@naver.com>
2019-12-01 16:13:54 +0900

    heroku

--- main.js
+++ main.js
@@ -57,6 +57,6 @@ app.use(function(error, req, res, next) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
