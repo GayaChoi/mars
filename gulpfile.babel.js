f3e36d0f1938f018c8276f455916202c2f00883d
GayaChoi <woosung827@naver.com>
2019-12-12 20:12:33 +0900

    yarn security alert bug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,51 @@
 import gulp from "gulp";
+import ws from "gulp-webserver";
+import del from "del";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+
+sass.compiler = require('node-sass');
+
+const routes = {
+    scss: {
+        watch: "gulp/scss/page/**/*.scss",
+        src: "gulp/scss/page/index.scss",
+        dest: "build/css"
+    }
+};
+
+const styles = () => 
+    gulp
+      .src(routes.scss.src)
+      .pipe(sass().on('error',sass.logError))
+      .pipe(gulp.dest(routes.scss.dest));
 
 
-export const dev = () => console.log("I will dev");
+const webserver = () => 
+   gulp.src('build').pipe(ws({ liveload: true, open: true }));
+
+
+const watch = () => {
+     gulp.watch(routes.scss.watch, styles); 
+};
+
+const prefixer = () => 
+     gulp
+     .src(routes.scss.src)
+     .pipe(autoprefixer({
+         cascade: false 
+     }))
+     .pipe(gulp.dest(routes.scss.dest)); 
+
+
+export const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([styles]);
+
+const autoPre = gulp.series([prefixer]);
+
+const live = gulp.parallel([webserver,watch]);
+
+export const dev = gulp.series([prepare, assets, live, autoPre]);
